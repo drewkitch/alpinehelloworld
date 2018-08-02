614aa317b304427bfcc787566c3108abc8e94a9f
Drew Kitch <drew.kitch@gmail.com>
2018-08-01 18:33:29 -0600

    file deployment

--- webapp/app.py
+++ webapp/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello world!'
+    return 'Hello world! A world without containers...'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
